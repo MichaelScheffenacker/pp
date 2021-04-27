404937dec344ea7b9dce4380183b7201fb9dfbde
Michael Scheffenacker <msc@formiculare.org>
2021-04-27 22:57:32 +0200

    added voting cards

--- static/main.js
+++ static/main.js
@@ -7,11 +7,18 @@ const main = function () {
     const nameInput = document.getElementById('name');
     const nameForm = document.getElementById('nameForm');
     const resultsDiv = document.getElementById('results');
+    const pointCards = document.querySelectorAll('#points .card');
     const url = document.baseURI;
 
     fetch(url + 'result')
         .then(res => res.json())
-        .then(users => addResult(users));
+        .then(users => addResult(users))
+        .catch(err => console.log(err));
+
+    pointCards.forEach(card => {
+        const points = card.innerHTML;
+        card.addEventListener('click', event => subPoints(event, points))
+    })
 
     function subName(event) {
         const name = nameInput.value;

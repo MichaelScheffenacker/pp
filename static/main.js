0fc8533667007ed70e82366adce73fdf60f417f8
Michael Scheffenacker <msc@formiculare.org>
2021-04-28 22:53:11 +0200

    added card selection rearranged html

--- static/main.js
+++ static/main.js
@@ -8,44 +8,62 @@ const main = function () {
     const nameForm = document.getElementById('nameForm');
     const resultsDiv = document.getElementById('results');
     const pointCards = document.querySelectorAll('#points .card');
+    const pointsDiv = document.getElementById('points');
     const url = document.baseURI;
-
-    fetch(url + 'result')
-        .then(res => res.json())
-        .then(users => addResult(users))
-        .catch(err => console.log(err));
 
     pointCards.forEach(card => {
         const points = card.innerHTML;
-        card.addEventListener('click', event => subPoints(event, points))
-    })
+        card.addEventListener('click', event => subPoints(event, points, card))
+    });
+
+    const update = function () {
+        fetch(url + 'result')
+            .then(res => res.json())
+            .then(users => addResult(users))
+            .catch(err => console.log(err));
+    }
+
+    window.setInterval(update, 2000);
 
     function subName(event) {
         const name = nameInput.value;
         event.preventDefault();
         fetch(url + 'user/' + name)
             .then(res => res.text())
-            .then(txt => console.log('Name submitted', txt))
+            .then(txt => {
+                console.log('Name submitted');
+                nameForm.classList.add('hidden');
+                resultsDiv.classList.remove('hidden');
+                pointsDiv.classList.remove('hidden');
+            })
             .catch(err => console.log(err));
     }
 
-    function subPoints(event, points) {
+    function subPoints(event, points, card) {
         const name = nameInput.value;
-        event.preventDefault();
         fetch(url + 'user/' + name + '/' + points)
             .then(res => res.text())
-            .then(txt => console.log('Points submitted', txt))
+            .then(txt => {
+                console.log('Points submitted');
+                pointCards.forEach(card => card.classList.remove('selected'))
+                card.classList.add('selected');
+            })
             .catch(err => console.log(err));
     }
 
     nameForm.addEventListener('submit', subName);
 
+    const results = [];
+
     function addResult(users) {
+        results.forEach(user => user.remove());
         for (const [name, pts] of Object.entries(users)) {
             const user = document.createElement('div');
             const text = document.createTextNode(`${name}: ${pts} Points`);
             user.appendChild(text);
+            results.push(user);
             resultsDiv.appendChild(user);
+
         }
     }
 }

b7ea722c1f75cd9c942bfb6abf46c9861f781319
Michael Scheffenacker <msc@formiculare.org>
2021-07-08 23:48:24 +0200

    seats getting filled

--- static/main.js
+++ static/main.js
@@ -7,6 +7,8 @@ const main = function () {
     const nameInput = document.getElementById('name');
     const nameForm = document.getElementById('nameForm');
     const resultsDiv = document.getElementById('results');
+    const seatsNorthDiv = document.getElementById('seats-north');
+    const seatsSouthDiv = document.getElementById('seats-south');
     const pointCards = document.querySelectorAll('#points .card');
     const pointsDiv = document.getElementById('points');
     const turnBtn = document.getElementById('turn');
@@ -61,21 +63,33 @@ const main = function () {
     nameForm.addEventListener('submit', subName);
 
     const results = [];
+    let switcher = true;
 
     function addResult(users) {
         results.forEach(user => user.remove());
+
         for (const [name, info] of Object.entries(users)) {
             if (name === nameInput.value && !info.voted) {
                 pointCards.forEach(card => card.classList.remove('selected'));
             }
-            const user = document.createElement('div');
+            const seatDiv = document.createElement('div');
+            const seatCardDiv = document.createElement('div');
+            seatDiv.classList.add('seat');
+            seatCardDiv.classList.add('card');
             const hasVoted = info.voted ? '🗸' : '';
             const pts = info.pts || '';
-            const text = document.createTextNode(`${name} ${hasVoted} ${pts}`);
-            user.appendChild(text);
-            results.push(user);
-            resultsDiv.appendChild(user);
-
+            const nameNode = document.createTextNode(name);
+            const voteNode = document.createTextNode(`${hasVoted} ${pts}`);
+            seatDiv.appendChild(nameNode);
+            seatDiv.appendChild(seatCardDiv);
+            seatCardDiv.appendChild(voteNode);
+            results.push(seatDiv);
+            if (switcher) {
+                seatsNorthDiv.appendChild(seatDiv);
+            } else {
+                seatsSouthDiv.appendChild(seatDiv);
+            }
+            switcher = !switcher;
         }
     }
 }
